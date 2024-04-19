1281cfad0e37a24e2a4ab0d9f82d80edb21dacb3
jeanluis0530 <107124646+jeanluis0530@users.noreply.github.com>
2024-04-18 20:05:07 -0400

    Update user.model.js

--- docs/models/user.model.js
+++ docs/models/user.model.js
@@ -1,1 +1,36 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  admin: {
+    type: Boolean,
+    default: false
+  },
+  saved: [],
+  photo: {
+    type: String,
+    default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+  },
+  courses: [],
+  sections: []
+},
+{
+  timestamps: true
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
 
